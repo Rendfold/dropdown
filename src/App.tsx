91a83f10655504219e3c0299e5abcbbfdf6b5eab
Levan Odishvili <leoodishvili@gmail.com>
2021-10-10 13:36:43 +0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Select } from './components';
-import { fakeApi } from '@lib/index';
+import { fakeApi } from '@lib';
 
 function App() {
   return (
